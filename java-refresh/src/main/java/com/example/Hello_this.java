e37a1d08bc56ece7cbb0fce9ccbc2d62af28f00e
Edward123452 <34315735+Edward123452@users.noreply.github.com>
2026-01-21 16:01:38 +0000

    Day another

---
+++ java-refresh/src/main/java/com/example/Hello_this.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Hello_this {
+    public static void main (String[] args){
+    String word1 = "Hello";
+    String word2 = " this is";
+    String word3 = " Me";
+    System.out.println( word1 + word2 + word3 );
+    }
+    
+}
